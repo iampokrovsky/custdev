90e54d6a952d6e54373d0bbbc85e2e276da336f9
Andrey Pokrovsky <iampokrovsky@gmail.com>
2020-04-01 13:08:40 +0300

    Абстрагирует элемент nav__cities в отдельный блок

--- build/js/script.js
+++ build/js/script.js
@@ -1,35 +1,35 @@
-var citiesList = document.querySelector(".nav__cities");
-var cities = document.querySelectorAll(".nav__city");
-var currentCity = document.querySelector(".nav__city--current");
+var citiesList = document.querySelector(".cities");
+var cities = document.querySelectorAll(".cities__item");
+var currentCity = document.querySelector(".cities__item--current");
 
 document.addEventListener("DOMContentLoaded", function () {
-  if (citiesList.classList.contains("nav__cities--no-js")) {
-    citiesList.classList.remove("nav__cities--no-js");
+  if (citiesList.classList.contains("cities--no-js")) {
+    citiesList.classList.remove("cities--no-js");
   }
 });
 
 
 currentCity.onmouseover = function (event) {
-  if (!citiesList.classList.contains("nav__cities--visible")) {
-    citiesList.classList.add("nav__cities--visible");
+  if (!citiesList.classList.contains("cities--visible")) {
+    citiesList.classList.add("cities--visible");
   }
 }
 
 citiesList.onmouseover = function (event) {
-  if (!citiesList.classList.contains("nav__cities--visible")) {
-    citiesList.classList.add("nav__cities--visible");
+  if (!citiesList.classList.contains("cities--visible")) {
+    citiesList.classList.add("cities--visible");
   }
 }
 
 currentCity.onmouseout = function (event) {
-  if (citiesList.classList.contains("nav__cities--visible")) {
-    citiesList.classList.remove("nav__cities--visible");
+  if (citiesList.classList.contains("cities--visible")) {
+    citiesList.classList.remove("cities--visible");
   }
 }
 
 citiesList.onmouseout = function (event) {
-  if (citiesList.classList.contains("nav__cities--visible")) {
-    citiesList.classList.remove("nav__cities--visible");
+  if (citiesList.classList.contains("cities--visible")) {
+    citiesList.classList.remove("cities--visible");
   }
 }
 
@@ -37,16 +37,14 @@ for (var i = 0; i < cities.length; i++) {
   cities[i].onclick = function (event) {
     event.preventDefault();
 
-    if (!this.classList.contains("nav__city--current")) {
-      console.log("Hello World!");
-
+    if (!this.classList.contains("cities__item--current")) {
       for (var n = 0; n < cities.length; n++) {
-        if (cities[n].classList.contains("nav__city--current")) {
-          cities[n].classList.remove("nav__city--current");
+        if (cities[n].classList.contains("cities__item--current")) {
+          cities[n].classList.remove("cities__item--current");
         }
       }
 
-      this.classList.add("nav__city--current");
+      this.classList.add("cities__item--current");
     }
   }
 }
